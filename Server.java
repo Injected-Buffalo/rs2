c91289223ae768fb10e953d2b6b6df152ae1e05b
Injected-Buffalo <cyndicaluniverse@hotmail.com>
2014-03-14 10:47:36 +0000

    Create Server.java
    Server.java is the main class of the whole rs2 framework this runs the cogs of the game.

---
+++ Server.java
@@ -0,0 +1,29 @@
+import java.util.Logger;
+
+public class Server implements Runnable {
+  
+  
+  private Logger logger = Logger.getAnonymousLogger();
+  public boolean running;
+  
+  private Thread thread; 
+  
+  public static void main(String[] args) {
+    start();
+  }
+  
+  public run() {
+  while (running) {
+  }
+  }
+  public start() {
+  new Thread();
+  logger.info("Running");
+  }
+  public stop() {
+  System.exit(0);
+  logger.info("Closing");
+  
+  }
+  
+  
